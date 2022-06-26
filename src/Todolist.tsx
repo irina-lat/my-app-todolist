ea273f8f8576630bd012b2764f622324664e54b7
irina-lat <irochka.bars@mail.ru>
2022-06-26 18:43:50 +0300

    add map

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 
-export function Todolist() {
+type todolistType = {
+    title: string,
+    tasks1: tasks1Type[],
+    // tasks2?: tasks2Type[]
+}
+
+type tasks1Type = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+// type tasks2Type = {
+//     id: number,
+//     title: string,
+//     isDone: boolean
+// }
+
+export function Todolist(props: todolistType) {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <input type="text"/>
             <button>+</button>
             <ul>
-                <li><input type="checkbox" checked={true}/><span>Html&Css</span></li>
-                <li><input type="checkbox" checked={true}/><span>Js</span></li>
-                <li><input type="checkbox" checked={false}/><span>React</span></li>
+                {props.tasks1.map((tasks1Array) => {
+                    return (
+                        <li key={tasks1Array.id} ><input type="checkbox" checked={tasks1Array.isDone}/><span>{tasks1Array.title}</span></li>
+                    )
+                })}
+
+                {/*<li><input type="checkbox" checked={true}/><span>Html&Css</span></li>*/}
+                {/*<li><input type="checkbox" checked={true}/><span>Js</span></li>*/}
+                {/*<li><input type="checkbox" checked={false}/><span>React</span></li>*/}
             </ul>
             <div>
                 <button>All</button>
