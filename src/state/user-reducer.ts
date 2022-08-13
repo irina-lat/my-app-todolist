29f02792cdaaf0d5dfc16f985cc03c84683f14da
irina-lat <irochka.bars@mail.ru>
2022-08-13 23:12:54 +0300

    add reducer

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -8,14 +8,22 @@ type ActionType = {
     [key: string]: any
 }
 
-export const userReducer = (state: StateType, action: ActionType) => {
+export const userReducer = (state: StateType, action: ActionType): StateType => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1
-            return state
+            let newState = {...state}
+            newState.age = state.age + 1
+            return newState
         case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1
-            return state
+            return {
+                ...state,
+                childrenCount: state.childrenCount + 1
+            }
+        case 'CHANGE-NAME':
+            return {
+                ...state,
+                name: action.newName
+            }
         default:
             throw new Error('I don\'t understand this type')
     }
